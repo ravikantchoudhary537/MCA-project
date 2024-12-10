97dce0549acf740e7c65067aa1ef67b54aa1c33f
Ravikant Choudhary <ravikantchoudhary537@gmail.com>
2024-12-10 16:32:15 +0530

    Access Tokens and Refresh Tokens added

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,5 +1,11 @@
 const express = require('express');
-const { registerUser, loginUser , getUserById, getUserDetails } = require('../controllers/userController');
+const {
+    registerUser,
+    loginUser,
+    getUserById,
+    getUserDetails,
+    refreshToken,
+} = require('../controllers/userController');
 const { validateToken } = require('../middleware/authMiddleware');
 
 const router = express.Router();
@@ -7,9 +13,8 @@ const router = express.Router();
 // Routes
 router.post('/register', registerUser);
 router.post('/login', loginUser);
-
-router.get('/getuserbytoken',validateToken,getUserDetails
-router.get('/getuserbyid' , getUserById)
-router.get('/getuserbytoken' , validateToken , getUserDetails)
+router.post('/refresh', refreshToken); // New route for refreshing tokens
+router.get('/getuserbyid', getUserById);
+router.get('/getuserbytoken', validateToken, getUserDetails);
 
 module.exports = router;
