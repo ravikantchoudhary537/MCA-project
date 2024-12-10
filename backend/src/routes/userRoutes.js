111da073b307210a3c5feb2acdbd1dee75230840
Ravikant Choudhary <ravikantchoudhary537@gmail.com>
2024-12-10 13:30:39 +0530

    getuserbytoken api

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { registerUser, loginUser } = require('../controllers/userController');
+const { registerUser, loginUser, getUserDetails } = require('../controllers/userController');
+const { validateToken } = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
 // Routes
 router.post('/register', registerUser);
 router.post('/login', loginUser);
-
+router.get('/getuserbytoken',validateToken,getUserDetails)
 module.exports = router;
